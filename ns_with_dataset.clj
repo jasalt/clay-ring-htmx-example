48f8c4d7719f3cf411ff266b7627737ee23dbcde
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-08-10 12:09:16 +0300

    Remove useless file

--- ns_with_dataset.clj
+++
@@ -1,6 +0,0 @@
-(ns ns-with-dataset
-  (:require [tablecloth.api :as tc])
-  )
-
-;; https://scicloj.github.io/tablecloth/#dataset-creation
-(defonce ds (tc/dataset "https://vega.github.io/vega-lite/examples/data/seattle-weather.csv"))
